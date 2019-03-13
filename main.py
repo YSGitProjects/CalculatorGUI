04840479c029bbbf2aabcc838c219574aa6c9ef2
YSUnilever <yasser.sheikh@unilever.com>
2019-03-13 23:22:36 +0000

    links

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def input_window():
         for j in range(3):
             b = (i*3 + j +1)
             Button(root, text = str(b), borderwidth = 1 ).grid(row = 4-i, column = j)
+            #https://www.tutorialspoint.com/python3/tk_grid.htm
     root.mainloop()
 print("hi")
 
