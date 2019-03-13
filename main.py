dc8c0a1980489077d6dee597d5c610328bccc88a
YSUnilever <yasser.sheikh@unilever.com>
2019-03-13 23:21:17 +0000

    main update

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-import tkinter
-top = tkinter.Tk()
+from tkinter import *
 
-top.mainloop()
 
-def calculator(*args):
-    
+
+
+def input_window():
+    root = Tk()
+    b = 0
+    for i in range(3):
+        for j in range(3):
+            b = (i*3 + j +1)
+            Button(root, text = str(b), borderwidth = 1 ).grid(row = 4-i, column = j)
+    root.mainloop()
+print("hi")
+
+input_window()
